4dd31c5faa2856f84a9014c881bd96fec5f6460f
Mark Hendriks <mark.hendriks@hybrit.org>
2021-01-06 10:23:46 +0100

    Update day 2

--- day-02/part-1.js
+++ day-02/part-1.js
@@ -1,21 +1,19 @@
 const { readLines } = require('../util.js');
 
-readLines('input').then(lines => {
-  let results = 
-  lines.map(lines => [...lines])
-       .map(chars => chars.reduce((map, c) => {
-              return map.set(c, (map.get(c) || 0) + 1);
-            }, new Map()))
-       .map(counts => {
-          let tt = {2:0, 3:0};
-          for (let value of counts.values()) {
-            if (value === 2) tt[2] = 1;
-            if (value === 3) tt[3] = 1;
-          }
-          return tt;
-       })
-       .reduce((acc, tt) => {
-        return {2: acc[2] + tt[2], 3: acc[3] + tt[3]};
-       }, {2: 0, 3: 0});
-  console.log(results[2] * results[3]);
-});
+let results = 
+readLines().map(lines => [...lines])
+     .map(chars => chars.reduce((map, c) => {
+            return map.set(c, (map.get(c) || 0) + 1);
+          }, new Map()))
+     .map(counts => {
+        let tt = {2:0, 3:0};
+        for (let value of counts.values()) {
+          if (value === 2) tt[2] = 1;
+          if (value === 3) tt[3] = 1;
+        }
+        return tt;
+     })
+     .reduce((acc, tt) => {
+      return {2: acc[2] + tt[2], 3: acc[3] + tt[3]};
+     }, {2: 0, 3: 0});
+console.log(results[2] * results[3]);
