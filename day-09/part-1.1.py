bf2d4036ddf9280fda36bbcb723c790a48b85420
Mark Hendriks <mark.hendriks@hybrit.org>
2021-01-10 23:57:24 +0100

    Update day 9

--- day-09/part-1.1.py
+++ day-09/part-1.1.py
@@ -1,34 +1,32 @@
 from collections import deque
 import sys
 
-with open('input') as f:
-  words = f.readlines()[0].split(" ")
-  players = int(words[0])
-  marbles = int(words[6])
-  current_player = 0
-  board = deque()
-  board.append(0)
-  position = 0
-  score = [0] * players
-  for marble in range(marbles):
-    marble += 1
-    if len(board) == 1:
-      board.append(marble)
-      position += 1
-    elif marble % 23 == 0:
-      a = (position - 7) % len(board)
-      score[current_player] += marble + board[a]
-      del [board[a]]
-      position = a
-    else:
-      a = ((position + 1) % len(board)) + 1
-      position = a
-      board.insert(position, marble)
-    current_player += 1
-    current_player %= players
-    sys.stdout.write('\r{i}/{j}'.format(i=marble,j=marbles))
-    sys.stdout.flush()
-  sys.stdout.write('\r           ')
+words = sys.stdin.readlines()[0].split(" ")
+players = int(words[0])
+marbles = int(words[6])
+current_player = 0
+board = deque()
+board.append(0)
+position = 0
+score = [0] * players
+for marble in range(marbles):
+  marble += 1
+  if len(board) == 1:
+    board.append(marble)
+    position += 1
+  elif marble % 23 == 0:
+    a = (position - 7) % len(board)
+    score[current_player] += marble + board[a]
+    del [board[a]]
+    position = a
+  else:
+    a = ((position + 1) % len(board)) + 1
+    position = a
+    board.insert(position, marble)
+  current_player += 1
+  current_player %= players
+  sys.stdout.write('\r{i}/{j}'.format(i=marble,j=marbles))
   sys.stdout.flush()
-  print(max(score))
-
+sys.stdout.write('\r           ')
+sys.stdout.flush()
+print(max(score))
--- day-09/part-2.py
+++ day-09/part-2.py
@@ -1,28 +1,27 @@
 import sys
 from collections import deque
 
-with open('input') as f:
-  words = f.readlines()[0].split(" ")
-  players = int(words[0])
-  marbles = int(words[6])
-  current_player = 0
-  board = deque()
-  board.append(0)
-  position = 0
-  score = [0] * players
-  for marble in range(marbles):
-    current_player = marble % players
-    marble += 1
-    if len(board) == 1:
-      board.append(marble)
-      position += 1
-    elif marble % 23 == 0:
-      board.rotate(7)
-      score[current_player] += marble + board.pop()
-      board.rotate(-1)
-    else:
-      board.rotate(-1)
-      board.append(marble)
 
-  print max(score)
+words = sys.stdin.readlines()[0].split(" ")
+players = int(words[0])
+marbles = int(words[6])
+current_player = 0
+board = deque()
+board.append(0)
+position = 0
+score = [0] * players
+for marble in range(marbles):
+  current_player = marble % players
+  marble += 1
+  if len(board) == 1:
+    board.append(marble)
+    position += 1
+  elif marble % 23 == 0:
+    board.rotate(7)
+    score[current_player] += marble + board.pop()
+    board.rotate(-1)
+  else:
+    board.rotate(-1)
+    board.append(marble)
 
+print(max(score))
