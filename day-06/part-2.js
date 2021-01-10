fcc1a8d48f15c8d4dbdfa54c5792e51402edb0a2
Mark Hendriks <mark.hendriks@hybrit.org>
2021-01-10 23:49:08 +0100

    Update day 6

--- day-06/part-2.js
+++ day-06/part-2.js
@@ -4,47 +4,44 @@ function manhattan(x, y, a, b) {
   return Math.abs(x - a) + Math.abs(y - b);
 }
 
-readLines('input')
-  .then(lines => {
-    let regions = lines
-      .map(line => line.split(', '))
-      .map(([a, b]) => [parseInt(a), parseInt(b)])
-      .reduce((regions, [x, y]) => {
-        regions.push({x, y, id: regions.length});
-        return regions;
-      }, []);
-    let minX = regions
-                .map(region => region.x)
-                .reduce((acc, x) => 
-                          Math.min(acc, x), 
-                        Number.POSITIVE_INFINITY);
-    let maxX = regions
-                .map(region => region.x)
-                .reduce((acc, x) =>
-                          Math.max(acc, x),
-                        Number.NEGATIVE_INFINITY);
-    let minY = regions
-                .map(region => region.y)
-                .reduce((acc, y) =>
-                          Math.min(acc, y),
-                        Number.POSITIVE_INFINITY);
-    let maxY = regions
-                .map(region => region.y)
-                .reduce((acc, y) =>
-                          Math.max(acc, y),
-                        Number.NEGATIVE_INFINITY);
-    let offset = 10000 / regions.length;
+let regions = readLines()
+  .map(line => line.split(', '))
+  .map(([a, b]) => [parseInt(a), parseInt(b)])
+  .reduce((regions, [x, y]) => {
+    regions.push({x, y, id: regions.length});
+    return regions;
+  }, []);
+let minX = regions
+            .map(region => region.x)
+            .reduce((acc, x) => 
+                      Math.min(acc, x), 
+                    Number.POSITIVE_INFINITY);
+let maxX = regions
+            .map(region => region.x)
+            .reduce((acc, x) =>
+                      Math.max(acc, x),
+                    Number.NEGATIVE_INFINITY);
+let minY = regions
+            .map(region => region.y)
+            .reduce((acc, y) =>
+                      Math.min(acc, y),
+                    Number.POSITIVE_INFINITY);
+let maxY = regions
+            .map(region => region.y)
+            .reduce((acc, y) =>
+                      Math.max(acc, y),
+                    Number.NEGATIVE_INFINITY);
+let offset = 10000 / regions.length;
 
-    let counter = 0;
-    for (let x = minX - offset; x <= maxX + offset; x++) {
-      for (let y = minY - offset; y <= maxY + offset; y++) {
-        let combined = regions.map(region => manhatten(x, y, region.x, region.y))
-                              .reduce((a, b) => a + b, 0);
-        if (combined < 10000) {
-          counter++;
-        }
-      }
+let counter = 0;
+for (let x = minX - offset; x <= maxX + offset; x++) {
+  for (let y = minY - offset; y <= maxY + offset; y++) {
+    let combined = regions.map(region => manhattan(x, y, region.x, region.y))
+                          .reduce((a, b) => a + b, 0);
+    if (combined < 10000) {
+      counter++;
     }
+  }
+}
 
-    console.log(counter);
-})
+console.log(counter);
