6745f33df5635765aec36bfeea3123f448d0c66a
Mark Hendriks <mark.hendriks@hybrit.org>
2021-01-10 23:57:13 +0100

    Update day 8

--- day-08/part-1.py
+++ day-08/part-1.py
@@ -1,16 +1,18 @@
-with open('input') as f:
-  line = f.readlines()[0]
-  numbers = map(lambda s: int(s), line.split(" "))
-  def recurse(numbers):
-    children = numbers[0]
-    meta = numbers[1]
-    offset = 2
-    result = 0
-    for child in range(children):
-      (r, o) = recurse(numbers[offset:])
-      offset += o
-      result += r
+import sys
 
-    return (result + sum(numbers[offset:offset+meta]), offset+meta)
 
-  print recurse(numbers)[0]
+line = sys.stdin.readlines()[0]
+numbers = list(map(lambda s: int(s), line.split(" ")))
+def recurse(numbers):
+  children = numbers[0]
+  meta = numbers[1]
+  offset = 2
+  result = 0
+  for child in range(children):
+    (r, o) = recurse(numbers[offset:])
+    offset += o
+    result += r
+
+  return (result + sum(numbers[offset:offset+meta]), offset+meta)
+
+print(recurse(numbers)[0])
--- day-08/part-2.py
+++ day-08/part-2.py
@@ -1,26 +1,27 @@
-with open('input') as f:
-  line = f.readlines()[0]
-  numbers = map(lambda s: int(s), line.split(" "))
-  def recurse(numbers):
-    children = numbers[0]
-    meta = numbers[1]
-    offset = 2
+import sys
 
-    values = []
-    for child in range(children):
-      (r, o) = recurse(numbers[offset:])
-      offset += o
-      values.append(r)
+line = sys.stdin.readlines()[0]
+numbers = list(map(lambda s: int(s), line.split(" ")))
+def recurse(numbers):
+  children = numbers[0]
+  meta = numbers[1]
+  offset = 2
 
-    metanumbers = numbers[offset:offset+meta]
-    result = 0
-    if children == 0:
-      result = sum(numbers[offset:offset+meta])
-    else:
-      for mn in metanumbers:
-        if mn >= 1 and mn <= len(values):
-          result += values[mn - 1]
+  values = []
+  for child in range(children):
+    (r, o) = recurse(numbers[offset:])
+    offset += o
+    values.append(r)
 
-    return (result, offset+meta)
+  metanumbers = numbers[offset:offset+meta]
+  result = 0
+  if children == 0:
+    result = sum(numbers[offset:offset+meta])
+  else:
+    for mn in metanumbers:
+      if mn >= 1 and mn <= len(values):
+        result += values[mn - 1]
 
-  print recurse(numbers)[0]
+  return (result, offset+meta)
+
+print(recurse(numbers)[0])
